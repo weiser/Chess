b624efb9a065f521ee803eb79386e062b74ffc7b
David Weiser <davidann@gmail.com>
2011-10-01 18:59:46 -0700

    added unit tests and prod code.

---
+++ src/ChessAnalysis.rb
@@ -0,0 +1,47 @@
+class ChessAnalysis
+  def analyzeBoard(boardConfig)
+    boardPlayers = getConfigByPlayer(boardConfig)
+    
+    locationOf_k = boardPlayers[:k][0]
+    locationOf_K = boardPlayers[:K][0]
+    
+    #remove the location of the kings so we only focus on where attacking players are
+    boardPlayers[:k] = nil
+    boardPlayers[:K] = nil
+    
+    #for each player, generate a list of all places they can attack
+    
+    boardPlayers.each{|playerType, playerAddresses|
+      playerAddresses.each{|address|
+        possibleMoves = generateMoves(player, address)
+        
+      }
+        
+    }
+    
+  end
+
+  def getConfigByPlayer(boardConfig)
+    boardRows = boardConfig.split
+    rowNum=1
+
+    boardPlayers = {}
+    boardRows.each { |row|
+      colNum = 0
+      row.split("").each{ |space|
+        colNum +=1
+        next unless space != '.'
+        if not  boardPlayers[space.to_sym].nil?
+          boardPlayers[space.to_sym].push([rowNum, colNum])
+        else
+
+          boardPlayers[space.to_sym] = [[rowNum, colNum]]
+        end
+
+      }
+      rowNum +=1
+
+    }
+    return boardPlayers
+  end
+end
--- test/ChessAnalysis_test.rb
+++ test/ChessAnalysis_test.rb
@@ -2,7 +2,31 @@ require 'test/unit'
 require '../src/ChessAnalysis'
 
 class ChessAnalysis_test < Test::Unit::TestCase
-  def testKingInCheck
-    assert_equal false, true
+  def getBoardConfig1
+    boardConfig = "..k.....\n"+
+    "ppp.pppp\n"+
+    "........\n"+
+    ".R...B..\n"+
+    "........\n"+
+    "........\n"+
+    "PPPPPPPP\n"+
+    "K.......\n"
   end
+  def testKingInCheck
+    
+    ca =  ChessAnalysis.new
+    kingsInCheck = ca.analyzeBoard(getBoardConfig1())
+  
+    assert_equal kingsInCheck[:k].nil?, true
+    assert_equal kingsInCheck[:K][:p], [7,2]
+  end
+  
+  def testAddressOfKings
+    ca = ChessAnalysis.new
+    boardConfig = ca.getConfigByPlayer(getBoardConfig1())
+    
+    assert_equal boardConfig[:k][0] == [1,3], true
+    assert_equal boardConfig[:K][0] == [8,1], true
+  end
+  
 end
